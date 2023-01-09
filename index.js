9944cc7a628af6acc2be6589d9a8bdddc0d75c35
bdidk235 <53656028+bdidk235@users.noreply.github.com>
2023-01-09 12:41:14 +0200

    Sussier

--- index.js
+++ index.js
@@ -5,14 +5,74 @@ let lastTimestamp = 0;
 var width = window.innerWidth;
 var height = window.innerHeight;
 
+// This is less likely to break
 var cubeSize = 50;
-var cubePos = [width / 2 - cubeSize / 2, height - cubeSize];
+var maxJumps = 2;
+var maxSpeed = 12;
+var friction = 8;
+var infJumps = false;
+
+// Game Stuff, Changing it may break it
+var cubePos = [width / 2 - cubeSize / 2, height / 2 - cubeSize];
 var gravity = 0;
 var gravityMult = 1;
 var move = 0;
 var moveMult = 1;
-var moving = false;
-var onFloor = true;
+var rightness = 0;
+var left = false;
+var right = false;
+var wasRight = true;
+var onFloor = false;
+var jumps = maxJumps;
+
+function lerp(start, end, amt) {
+	return (1 - amt) * start + amt * end
+}
+
+function stableTime() {
+	return Math.max(deltaTime, 0)
+}
+
+function movement() {
+	// Movement
+    cubePos[0] += move * moveMult * stableTime();
+
+    if (left || right) {
+		wasRight = right;
+        moveMult = lerp(moveMult, maxSpeed, stableTime() / maxSpeed);
+    } else {
+        moveMult = lerp(moveMult, 0, stableTime() / 8);
+    }
+
+	abus = wasRight ? 0 : 1;
+	rightness = lerp(rightness, abus, Math.max(stableTime() / 4, 1));
+
+    cubePos[0] = Math.min(Math.max(cubePos[0], 0), width - cubeSize);
+
+	if (cubePos[0] < 0 || cubePos[0] > width - cubeSize)
+		moveMult = 0;
+
+    onFloor = cubePos[1] >= height - cubeSize;
+
+	// Gravity
+    if (!onFloor) {
+        gravity -= gravityMult * stableTime() * 0.2;
+        gravityMult = lerp(gravityMult, 4, stableTime() / Math.max(moveMult, 0.1) / 32);
+    } else {
+		jumps = maxJumps;
+        gravity = 0;
+        gravityMult = 1;
+    }
+
+    if (cubePos[1] > height - cubeSize) {
+        cubePos[1] -= (cubePos[1] - (height - cubeSize)) * stableTime() / 6;
+        if (cubePos[1] < (height - cubeSize) + 1) cubePos[1] = height - cubeSize;
+    }
+
+    cubePos[1] -= gravity * stableTime();
+
+	console.log(cubePos);
+}
 
 function update(timestamp) {
     requestAnimationFrame(update);
@@ -23,33 +83,7 @@ function update(timestamp) {
     width = window.innerWidth;
     height = window.innerHeight;
 
-    cubePos[0] += move * moveMult * deltaTime;
-
-    if (moving) {
-        moveMult = Math.min(moveMult + 0.1 * deltaTime, 16);
-    } else {
-        move *= Math.min(deltaTime * 0.25, 1);
-        moveMult *= Math.min(deltaTime * 0.25, 1);
-    }
-
-    cubePos[0] = Math.min(Math.max(cubePos[0], 0), width - cubeSize)
-
-    onFloor = cubePos[1] >= height - cubeSize;
-
-    if (!onFloor) {
-        gravity -= gravityMult * deltaTime * 0.2;
-        gravityMult = Math.min(gravityMult + 0.1 * deltaTime, 8)
-    } else {
-        gravity = 0;
-        gravityMult = 1;
-    }
-
-    if (cubePos[1] > height - cubeSize) {
-        cubePos[1] -= (cubePos[1] - (height - cubeSize)) * deltaTime / 6;
-        if (cubePos[1] < (height - cubeSize) + 1) cubePos[1] = height - cubeSize;
-    }
-
-    cubePos[1] -= gravity * deltaTime;
+    movement();
 
     if (canvas.getContext) {
         const ctx = canvas.getContext("2d");
@@ -63,7 +97,7 @@ function update(timestamp) {
         ctx.fillStyle = "rgb(255, 0, 0)";
         ctx.fillRect(-cubeSize / 2, -cubeSize / 2, cubeSize, cubeSize);
         ctx.fillStyle = "rgb(0, 255, 255)";
-        ctx.fillRect(0, -cubeSize / 4, cubeSize / 2, cubeSize / 4);
+		ctx.fillRect(lerp(0, -cubeSize / 2, rightness), -cubeSize / 4, lerp(cubeSize / 2, cubeSize / 2, rightness), cubeSize / 4);
 
         ctx.setTransform(1, 0, 0, 1, 0, 0);
     }
@@ -74,20 +108,37 @@ window.onload = function () {
 
     document.onkeydown = function (event) {
         if (event.code === "Space") {
-            if (onFloor) {
-                cubePos[1] = height - cubeSize - 1;
-                gravity += 9;
+            if (jumps > 0) {
+				jumps -= 1;
+				if (jumps < 1 && maxJumps > 1)
+					moveMult *= 3;
+					if (infJumps)
+						jumps = maxJumps;
+				if (onFloor)
+					cubePos[1] = height - cubeSize - 1;
+                gravity = 9;
             }
-        } else if (event.code === "KeyA") {
-            moving = true;
-            move = -1;
-        } else if (event.code === "KeyD") {
-            moving = true;
-            move = 1;
+        }
+		if (event.code === "KeyA") {
+			left = true;
+			move = -1;
+        }
+		if (event.code === "KeyD") {
+			right = true;
+			move = 1;
         }
     };
 
     document.onkeyup = function (event) {
-        if (event.code === "KeyA" || event.code === "KeyD") moving = false;
+        if (event.code === "KeyA") {
+			left = false;
+			if (right)
+				move = 1;
+		}
+		if (event.code === "KeyD") {
+			right = false;
+			if (left)
+				move = -1;
+		}
     };
 }
